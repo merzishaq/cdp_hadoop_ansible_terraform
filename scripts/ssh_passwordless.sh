249cefe222172ad45b3bc61c9da8911cd9810d53
merzishaq <merzishaq@gmail.com>
2024-09-02 14:03:09 +0200

    fix: ssh passwordless script

--- scripts/ssh_passwordless.sh
+++ scripts/ssh_passwordless.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 mkdir -p /home/universaluser/.ssh
 chmod 700 /home/universaluser/.ssh
+touch /home/universaluser/.ssh/authorized_keys
 cp ../ssh_keys/id_rsa.pub /home/universaluser/.ssh/authorized_keys
 chmod 600 /home/universaluser/.ssh/authorized_keys
 chown -R universaluser:universaluser /home/universaluser/.ssh
